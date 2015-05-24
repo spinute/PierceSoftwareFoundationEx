d82301f597d169611357ad30d8e1515f41650eeb
spinute <spinutids@gmail.com>
2015-05-25 00:11:50 +0900

    blt_nat.v

---
+++ blt_nat.v
@@ -0,0 +1,11 @@
+Definition blt_nat (n m : nat) : bool :=
+ match (minus m n) with
+ | S n' => true
+ | O => false
+end.
+
+Example test_blt_nat1: (blt_nat 2 2) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat2: (blt_nat 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat3: (blt_nat 4 2) = false.
